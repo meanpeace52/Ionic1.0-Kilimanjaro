b353952678302e0e571078030bf3be7e54e8be01
solar-dev23 <solar.dev023@gmail.com>
2016-12-30 13:56:45 +0530

    worked on the social sharing plugin and fix

--- www/js/directives.js
+++ www/js/directives.js
@@ -3,13 +3,13 @@ angular.module('app.directives', [])
 
         .directive('customBackButton', ['$ionicHistory', function($ionicHistory) {
                 return{
-                    link: function(scope, elem){
-                        elem.bind('click', function(){
+                    link: function(scope, elem) {
+                        elem.bind('click', function() {
                             $ionicHistory.goBack();
                         })
                     }
                 }
-        }])
+            }])
         .directive('hideTabs', function($rootScope, $ionicTabsDelegate) {
             return {
                 restrict: 'A',
@@ -22,4 +22,56 @@ angular.module('app.directives', [])
                     });
                 }
             };
-        });
+        })
+
+        .directive('nativeShare', ['sharedUtils', function(sharedUtils) {
+                return {
+                    restrict: 'A',
+                    link: function(scope, elem, attr) {
+                        var b64Data = false;
+                        scope.$on('$destroy', function() {
+                            b64Data = null;
+                        })
+                        function nativeShare(options) {
+                            if (window.cordova && window.plugins && window.plugins.socialsharing) {
+                                window.plugins.socialsharing.shareWithOptions(options);
+                            }
+                        }
+                        elem.bind('click', function() {
+                            var options = {};
+                            if (attr.subject) {
+                                options.subject = attr.subject;
+                            }
+                            if (attr.message) {
+                                options.message = attr.message;
+                            }
+                            if (attr.files) {
+                                options.files = [attr.files];
+                            }
+                            if (b64Data) {
+                                options.files = [b64Data]
+                            }
+                            if (attr.files && !b64Data) {
+                                sharedUtils.showLoading();
+                                var img = new Image;
+                                img.setAttribute('crossOrigin', 'anonymous');
+                                img.src = attr.files;
+                                img.onload = function() {
+                                    sharedUtils.hideLoading();
+                                    var c = document.createElement('canvas');
+                                    c.width = img.width;
+                                    c.height = img.height;
+                                    var x = c.getContext('2d');
+                                    x.drawImage(img, 0, 0);
+                                    var imgBase64 = c.toDataURL();
+                                    options.files = imgBase64;
+                                    b64Data = imgBase64;
+                                    nativeShare(options);
+                                }
+                            } else {
+                                nativeShare(options);
+                            }
+                        })
+                    }
+                };
+            }]);
